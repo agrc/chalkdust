d0fad8a2c96966417db6aac85311c15c425ed8af
steveoh <sgourley@utah.gov>
2016-02-10 17:10:28 -0700

    dont' publish stylus files

--- GruntFile.js
+++ GruntFile.js
@@ -20,6 +20,7 @@ module.exports = function(grunt) {
         '!**/*.uncompressed.js',
         '!**/*consoleStripped.js',
         '!**/tests/**',
+        '!**/*.styl',
         '!build-report.txt',
         '!components-jasmine/**',
         '!favico.js/**',
