4ac8149678fcb1ec3fd0fdddc9a1acc76e4f546e
steveoh <sgourley@utah.gov>
2021-06-29 19:26:50 -0600

    feat: add linting to vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import reactJsx from "vite-react-jsx";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import reactJsx from 'vite-react-jsx';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx()],
+  plugins: [reactRefresh(), reactJsx(), eslintPlugin()],
 });
