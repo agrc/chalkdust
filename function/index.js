4f25efeaf1492902984d7985270fba350fd82fcd
steveoh <sgourley@utah.gov>
2021-07-07 10:14:26 -0600

    fix: only use dotenv in development

--- function/index.js
+++ function/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const client = require('@sendgrid/client');
 const yup = require('yup');
