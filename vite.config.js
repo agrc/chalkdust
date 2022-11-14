5dd7e893cf2df0ba18af16575f98bb3ca5bf4192
steveoh <sgourley@utah.gov>
2022-11-14 13:39:52 -0700

    chore: updates for vite 3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import reactJsx from 'vite-react-jsx';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx(), eslintPlugin()],
+  plugins: [react(), eslintPlugin()],
 });
