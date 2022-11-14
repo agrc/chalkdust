1646a792c740f1a64d133d359bf2d450331b46b2
steveoh <sgourley@utah.gov>
2022-11-14 14:21:19 -0700

    feat: load version for header

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import loadVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin(), loadVersion()],
 });
