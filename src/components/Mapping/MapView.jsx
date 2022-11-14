45e5a242d27da752a25cccef3537a99579062308
steveoh <sgourley@utah.gov>
2022-11-14 14:23:11 -0700

    refactor: replace deprecated package

--- src/components/Mapping/MapView.jsx
+++ src/components/Mapping/MapView.jsx
@@ -1,13 +1,13 @@
-import { useRef, useEffect } from 'react';
-import MapView from '@arcgis/core/views/MapView';
-import WebMap from '@arcgis/core/WebMap';
-import Viewpoint from '@arcgis/core/Viewpoint';
+import { whenOnce } from '@arcgis/core/core/reactiveUtils';
 import Point from '@arcgis/core/geometry/Point';
 import Graphic from '@arcgis/core/Graphic';
-import { marker } from './Symbols';
-import { ProjectUtmToWgs } from './Projections';
-import { whenTrueOnce } from '@arcgis/core/core/watchUtils';
+import Viewpoint from '@arcgis/core/Viewpoint';
+import MapView from '@arcgis/core/views/MapView';
+import WebMap from '@arcgis/core/WebMap';
 import queryString from 'query-string';
+import { useEffect, useRef } from 'react';
+import { ProjectUtmToWgs } from './Projections.jsx';
+import { marker } from './Symbols.jsx';
 
 const ReactMapView = () => {
   const mapDiv = useRef(null);
@@ -29,13 +29,10 @@ const ReactMapView = () => {
       });
     }
 
-    console.log('parsing query string');
     let { redline: redlineString } = queryString.parse(location.search);
     let redline;
 
     if (redlineString) {
-      console.log('parsing redline point');
-
       let redlineJson = JSON.parse(redlineString);
 
       if (typeof redlineJson === 'string') {
@@ -49,18 +46,12 @@ const ReactMapView = () => {
       });
 
       if (redline.spatialReference.wkid === 26912) {
-        console.log('projecting redline');
-
         redline = ProjectUtmToWgs(redline);
       }
     }
 
-    console.log('zooming to viewpoint');
-
     if (redline) {
-      whenTrueOnce(mapView.current, 'ready', () => {
-        console.log('view is ready, zooming');
-
+      whenOnce(() => mapView.current.ready).then(() => {
         mapView.current.goTo(
           new Viewpoint({
             scale: 6000,
