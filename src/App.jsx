f21f81ee61d9306dcef40f755841246ed9b74dce
steveoh <sgourley@utah.gov>
2024-06-12 15:04:21 -0600

    refactor: remove reactfire

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { getAnalytics } from 'firebase/analytics';
 import PropTypes from 'prop-types';
 import { ErrorBoundary } from 'react-error-boundary';
-import { AnalyticsProvider, useFirebaseApp } from 'reactfire';
 import Header from './components/Header';
 import MapView from './components/Mapping/MapView.jsx';
 
@@ -20,19 +18,14 @@ ErrorFallback.propTypes = {
 };
 
 function App() {
-  const app = useFirebaseApp();
-  const analytics = getAnalytics(app);
-
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <AnalyticsProvider sdk={analytics}>
-        <div className="w-screen h-screen">
-          <Header title="Chalkdust Redline Viewer" version={version ?? '1.0.0'} />
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <MapView />
-          </ErrorBoundary>
-        </div>
-      </AnalyticsProvider>
+      <div className="w-screen h-screen">
+        <Header title="Chalkdust Redline Viewer" version={version ?? '1.0.0'} />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <MapView />
+        </ErrorBoundary>
+      </div>
     </ErrorBoundary>
   );
 }
